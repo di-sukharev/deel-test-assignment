de285c2ec90ac72ab52d0c9e387856742766afed
dmitry.sukharev <dmitry.sukharev@alibaba-inc.com>
2022-03-20 16:45:01 +0400

    added comments to api.ts

--- src/api.ts
+++ src/api.ts
@@ -3,6 +3,8 @@ import citiesMock from "./citiesMock.json";
 export const fetchAutoCompleteCities = async (
   query: string
 ): Promise<string[]> => {
+  // this is a simulation of a real API request
+  // lets imagine that this api takes query and filters cities
   return Promise.resolve(
     citiesMock.filter(
       (suggestion) => suggestion.toLowerCase().indexOf(query.toLowerCase()) > -1
